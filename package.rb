967a4571c948c9867a07a7651fa519274ef8d565
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2018-09-12 08:43:19 +0100

    Delete puts on package

--- package.rb
+++ package.rb
@@ -24,32 +24,26 @@ class Package
       byte = @data[index]
       case byte
       when DataTag::RELOJ_DE_PARTIDO_CON_DECIMAS_OPCIONALES
-        puts "RELOJ_DE_PARTIDO_CON_DECIMAS_OPCIONALES"
-
         msg << "RELOJ_DE_PARTIDO_CON_DECIMAS_OPCIONALES = "
         msg << Trama.new(@data[index + 1..index + DataTagOffset::RELOJ_DE_PARTIDO_CON_DECIMAS_OPCIONALES]).to_s
         msg << "\n"
 
       when DataTag::RELOJ_DE_POSESION
-        puts "RELOJ_DE_POSESION"
         msg << "RELOJ_DE_POSESION = "
         msg << Trama.new(@data[index + 1..index + 4]).to_s
         msg << "\n"
 
       when DataTag::PERIODO
-        puts "PERIODO"
         msg << "PERIODO = "
         msg << Trama.new(@data[index + 1..index + 1]).to_s
         msg << "\n"
 
       when DataTag::PUNTOS_LOCAL
-        puts "PUNTOS_LOCAL"
         msg << "PUNTOS_LOCAL = "
         msg << Trama.new(@data[index + 1..index + 3]).to_s
         msg << "\n"
 
       when DataTag::PUNTOS_VISITANTE
-        puts "PUNTOS_VISITANTE"
         msg << "PUNTOS_VISITANTE = "
         msg << Trama.new(@data[index + 1..index + 3]).to_s
         msg << "\n"
